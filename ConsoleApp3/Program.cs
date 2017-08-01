0200d6baee6edecea7dd98ff895f93226c0f8dfe
JavierUribeMontoyaEngineer <javier.uribe@coitimur.org>
2017-08-01 20:59:35 +0200

    Caso de desuscribir al oyente

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleApp3
         {
             var caja = new Caja() { };
             caja.Total += Caja_Total;
-            // caja.Total -= Caja_Total;
             int id = 2;
             Console.WriteLine("Caso de ticket con 20 euros(no debe avisar a nadie)");
             decimal importe = 20;
@@ -24,6 +23,11 @@ namespace ConsoleApp3
 
             Console.WriteLine("Caso de añadir 50 cuando ya habia superado 200(no debe avisar porque se pone a 0)");
             importe = 50;
+            caja.addTicket(id, importe);
+
+            Console.WriteLine("Caso de desuscribir al oyente. No importa ahora el importe y no debe oir");
+            importe = 200;
+            caja.Total -= Caja_Total;
             caja.addTicket(id, importe);
 
             Console.ReadLine();
